cb5a6cb570a7ae5e96ee31beda4c64bdf362816a
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-14 17:16:16 +0200

    feat:formData author title body public

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 
 function App() {
+  //DATI
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
+
   return <></>;
 }
 
